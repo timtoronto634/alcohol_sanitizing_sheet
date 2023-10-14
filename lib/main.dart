53887de31f78dddb6c4e52ba35dd85af6a04cd94
kenoji <tanikei0818@gmail.com>
2023-10-14 15:25:29 +0900

    叩ける

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:dart_openai/dart_openai.dart';
 
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
@@ -16,6 +17,19 @@ void main() async {
 
   await dotenv.load(fileName: '.env');
 
+  OpenAI.apiKey = dotenv.get('OPENAI_API_KEY');
+  /*
+  OpenAIChatCompletionModel chatCompletion = await OpenAI.instance.chat.create(
+    model: "gpt-3.5-turbo",
+    messages: [
+      OpenAIChatCompletionChoiceMessageModel(
+        content: "hello, what is Flutter and Dart ?",
+        role: OpenAIChatMessageRole.user,
+      ),
+    ],
+  );
+  print(chatCompletion.choices.first.message);
+  */
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
