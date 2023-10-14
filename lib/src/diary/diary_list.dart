37382404e7c62c7ae382f0bd6352abc58f6e1a2b
timtoronto634 <tim225x@gmail.com>
2023-10-14 14:39:50 +0900

    feat add refresh of diaries

--- lib/src/diary/diary_list.dart
+++ lib/src/diary/diary_list.dart
@@ -1,49 +1,64 @@
 import 'package:alcohol_sanitizing_sheet/src/diary/diary.dart';
+import 'package:alcohol_sanitizing_sheet/src/helper.dart/db_helper.dart';
 import 'package:flutter/material.dart';
 
-class DiaryList extends StatelessWidget {
-  final List<Diary> diaryEntries = [
-    Diary(
-        id: 1,
-        title: 'Day at the Beach',
-        content: 'Had an amazing time at the beach with friends.',
-        date: DateTime(2023, 1, 1)),
-    Diary(
-        id: 2,
-        title: 'Flutter Study',
-        content: 'Learned a lot about state management today.',
-        date: DateTime(2023, 1, 2)),
-    Diary(
-        id: 3,
-        title: 'Grocery Shopping',
-        content: 'Bought vegetables, fruits, and some snacks.',
-        date: DateTime(2023, 1, 3)),
-    Diary(
-        id: 4,
-        title: 'Workout',
-        content: 'Did a 30-minute cardio session.',
-        date: DateTime(2023, 1, 4)),
-    Diary(
-        id: 5,
-        title: 'Reading',
-        content: 'Read a few chapters of a new novel.',
-        date: DateTime(2023, 1, 5)),
-  ];
+class DiaryList extends StatefulWidget {
+  @override
+  _DiaryListState createState() => _DiaryListState();
+}
+
+class _DiaryListState extends State<DiaryList> {
+  late Future<List<Diary>> diaryList;
+
+  Future<List<Diary>> fetchDiariesFromDB() async {
+    // DBHelper dbHelper = DBHelper();
+    return await DBHelper.fetchDiaries();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    diaryList = fetchDiariesFromDB();
+  }
+
+  Future<void> _reloadData() async {
+    setState(() {
+      diaryList = fetchDiariesFromDB();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: diaryEntries.length,
-      itemBuilder: (context, index) {
-        final diary = diaryEntries[index];
-        return ListTile(
-          title: Text(diary.title),
-          subtitle: Text(diary.date.toIso8601String()),
-          onTap: () {
-            // Navigate to diary details page
-          },
-        );
-      },
+    return RefreshIndicator(
+      onRefresh: _reloadData,
+      child: FutureBuilder<List<Diary>>(
+        future: fetchDiariesFromDB(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator(); // show a loading spinner
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Text('No diaries found');
+          } else {
+            List<Diary> diaries = snapshot.data!;
+
+            return ListView.builder(
+              itemCount: diaries.length,
+              itemBuilder: (context, index) {
+                final diary = diaries[index];
+                return ListTile(
+                  title: Text(diary.title),
+                  subtitle: Text(diary.date.toIso8601String()),
+                  onTap: () {
+                    // Navigate to diary details page
+                  },
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
