f21ecd8345608464e374d5e50a36c750bea8fac5
Masaya Kibayashi <kibayashi.masaya@gmail.com>
2023-10-14 15:34:20 +0900

    いらんインポート削除

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,13 +2,6 @@ import 'package:alcohol_sanitizing_sheet/src/diary/diary_create.dart';
 import 'package:alcohol_sanitizing_sheet/src/diary/diary_list.dart';
 import 'package:alcohol_sanitizing_sheet/src/summary/summary_create.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-
-import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
-
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MyApp extends StatelessWidget {
   @override
