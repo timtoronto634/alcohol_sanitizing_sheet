04bea46c5c28ea052e31afd81435d9b1d0280b5c
timtoronto634 <tim225x@gmail.com>
2023-10-14 14:26:38 +0900

    add print and dummy data

--- lib/src/diary/diary_list.dart
+++ lib/src/diary/diary_list.dart
@@ -2,9 +2,32 @@ import 'package:alcohol_sanitizing_sheet/src/diary/diary.dart';
 import 'package:flutter/material.dart';
 
 class DiaryList extends StatelessWidget {
-  // This data would usually come from your database
   final List<Diary> diaryEntries = [
-    // Mocked data or fetch from the database
+    Diary(
+        id: 1,
+        title: 'Day at the Beach',
+        content: 'Had an amazing time at the beach with friends.',
+        date: DateTime(2023, 1, 1)),
+    Diary(
+        id: 2,
+        title: 'Flutter Study',
+        content: 'Learned a lot about state management today.',
+        date: DateTime(2023, 1, 2)),
+    Diary(
+        id: 3,
+        title: 'Grocery Shopping',
+        content: 'Bought vegetables, fruits, and some snacks.',
+        date: DateTime(2023, 1, 3)),
+    Diary(
+        id: 4,
+        title: 'Workout',
+        content: 'Did a 30-minute cardio session.',
+        date: DateTime(2023, 1, 4)),
+    Diary(
+        id: 5,
+        title: 'Reading',
+        content: 'Read a few chapters of a new novel.',
+        date: DateTime(2023, 1, 5)),
   ];
 
   @override
--- lib/src/helper.dart/db_helper.dart
+++ lib/src/helper.dart/db_helper.dart
@@ -4,6 +4,7 @@ import 'package:path/path.dart';
 
 class DBHelper {
   static Future<Database> database() async {
+    print("database");
     final dbPath = await getDatabasesPath();
     return openDatabase(join(dbPath, 'myDiary.db'), onCreate: (db, version) {
       return db.execute(
@@ -14,6 +15,7 @@ class DBHelper {
 
   // Method to insert a new diary entry (Now static)
   static Future<void> insertDiary(Diary diary) async {
+    print("insertDiary");
     final db = await DBHelper.database();
     await db.insert('diaries', diary.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace);
@@ -21,6 +23,7 @@ class DBHelper {
 
   // Method to fetch all diaries (Now static)
   static Future<List<Diary>> fetchDiaries() async {
+    print("fetchDiaries");
     final db = await DBHelper.database();
     final result = await db.query('diaries');
     return result.map((e) => Diary.fromMap(e)).toList();
