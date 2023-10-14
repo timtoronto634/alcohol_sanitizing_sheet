63eafe89ac6b605c35a0ececc6cce4ea9226ed43
kenoji <tanikei0818@gmail.com>
2023-10-14 18:08:42 +0900

    読み上げるぜ

--- lib/src/summary/summary_create.dart
+++ lib/src/summary/summary_create.dart
@@ -3,6 +3,8 @@ import 'dart:ffi';
 import 'package:alcohol_sanitizing_sheet/src/helper.dart/db_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:dart_openai/dart_openai.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Summary extends StatefulWidget {
   @override
@@ -39,10 +41,30 @@ class SummaryState extends State<Summary> {
       ],
     );
     print(chatCompletion.usage);
+    final messageContent = chatCompletion.choices.first.message.content;
+    talk(messageContent, speakerName: "aoi_emo", speed: 3.0);
     setState(() {
-      _summary = chatCompletion.choices.first.message.content;
+      _summary = messageContent;
       _isLoading = false;
     });
+  }
+
+  void talk(String message,
+      {String speakerName = "akane_west_emo", double speed = 1.0}) async {
+    const baseUrl = 'https://webapi.aitalk.jp/webapi/v5/ttsget.php';
+    const ext = 'mp3';
+    final username = dotenv.get('AITALK_USERNAME');
+    final password = dotenv.get('AITALK_PASSWORD');
+
+    var speakerName = 'akane_west_emo';
+    var text = message;
+    var speedStr = speed.toString();
+
+    var url =
+        "$baseUrl?username=$username&password=$password&ext=$ext&speaker_name=$speakerName&speed=$speedStr&text=$text";
+    final player = AudioPlayer();
+    await player.setUrl(url);
+    player.play();
   }
 
   @override
