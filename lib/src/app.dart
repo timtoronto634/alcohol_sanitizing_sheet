0f2f0f240db2ac2cc8e5d781cf2b125acbc5217b
Lettuce222 <k.miyachi222@gmail.com>
2023-10-14 14:48:15 +0900

    feat: add tabbar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -8,28 +8,44 @@ import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
 
 class MyApp extends StatelessWidget {
-  final SettingsController settingsController;
-
-  MyApp({required this.settingsController});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyDiaryHomePage(settingsController: settingsController),
+      home: MyDiaryHomePage(title: 'MyDiaryHomePage'),
     );
   }
 }
 
-class MyDiaryHomePage extends StatelessWidget {
-  final SettingsController settingsController;
+class MyDiaryHomePage extends StatefulWidget {
+  MyDiaryHomePage({Key? key, required this.title}) : super(key: key);
 
-  MyDiaryHomePage({required this.settingsController});
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyDiaryHomePage> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    DiaryList(),
+    const Text(
+      'Index 1: Business',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('MyDiary')),
-      body: DiaryList(), // This will be the list of diaries
+      body: _widgetOptions
+          .elementAt(_selectedIndex), // This will be the list of diaries
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -39,85 +55,21 @@ class MyDiaryHomePage extends StatelessWidget {
         },
         child: Icon(Icons.add),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
-
-
-// /// The Widget that configures your application.
-// class MyApp extends StatelessWidget {
-//   const MyApp({
-//     super.key,
-//     required this.settingsController,
-//   });
-
-//   final SettingsController settingsController;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // Glue the SettingsController to the MaterialApp.
-//     //
-//     // The ListenableBuilder Widget listens to the SettingsController for changes.
-//     // Whenever the user updates their settings, the MaterialApp is rebuilt.
-//     return ListenableBuilder(
-//       listenable: settingsController,
-//       builder: (BuildContext context, Widget? child) {
-//         return MaterialApp(
-//           // Providing a restorationScopeId allows the Navigator built by the
-//           // MaterialApp to restore the navigation stack when a user leaves and
-//           // returns to the app after it has been killed while running in the
-//           // background.
-//           restorationScopeId: 'app',
-
-//           // Provide the generated AppLocalizations to the MaterialApp. This
-//           // allows descendant Widgets to display the correct translations
-//           // depending on the user's locale.
-//           localizationsDelegates: const [
-//             AppLocalizations.delegate,
-//             GlobalMaterialLocalizations.delegate,
-//             GlobalWidgetsLocalizations.delegate,
-//             GlobalCupertinoLocalizations.delegate,
-//           ],
-//           supportedLocales: const [
-//             Locale('en', ''), // English, no country code
-//           ],
-
-//           // Use AppLocalizations to configure the correct application title
-//           // depending on the user's locale.
-//           //
-//           // The appTitle is defined in .arb files found in the localization
-//           // directory.
-//           onGenerateTitle: (BuildContext context) =>
-//               AppLocalizations.of(context)!.appTitle,
-
-//           // Define a light and dark color theme. Then, read the user's
-//           // preferred ThemeMode (light, dark, or system default) from the
-//           // SettingsController to display the correct theme.
-//           theme: ThemeData(),
-//           darkTheme: ThemeData.dark(),
-//           themeMode: settingsController.themeMode,
-
-//           // Define a function to handle named routes in order to support
-//           // Flutter web url navigation and deep linking.
-//           onGenerateRoute: (RouteSettings routeSettings) {
-//             return MaterialPageRoute<void>(
-//               settings: routeSettings,
-//               builder: (BuildContext context) {
-//                 switch (routeSettings.name) {
-//                   case SettingsView.routeName:
-//                     return SettingsView(controller: settingsController);
-//                   case SampleItemDetailsView.routeName:
-//                     return const SampleItemDetailsView();
-//                   case SampleItemListView.routeName:
-//                   default:
-//                     return const SampleItemListView();
-//                 }
-//               },
-//             );
-//           },
-//         );
-//       },
-//     );
-//   }
-// }
-
