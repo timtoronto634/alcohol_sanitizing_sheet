4df53481ca9f1ade1c3cb4cc6b607c2e71fb78f2
higurashi-takuto <contact@hgrs.me>
2023-10-14 16:16:00 +0900

    ナビバーのアクティブカラーを変更

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -103,7 +103,7 @@ class _MyDiaryHomePageState extends State<MyDiaryHomePage> {
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Colors.blue[800],
         onTap: _onItemTapped,
       ),
     );
