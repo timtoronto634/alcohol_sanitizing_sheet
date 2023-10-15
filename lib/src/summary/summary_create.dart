8b68ce7b8e5c84f56ee2c559a219f8f6efd8adf5
timtoronto634 <tim225x@gmail.com>
2023-10-15 10:26:54 +0900

    move button

--- lib/src/summary/summary_create.dart
+++ lib/src/summary/summary_create.dart
@@ -82,20 +82,6 @@ class SummaryState extends State<Summary> {
         children: <Widget>[
           Column(
             children: [
-              ElevatedButton(
-                  onPressed: _onPressed,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromRGBO(219, 171, 188, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(1000),
-                    ),
-                  ),
-                  child: const Text(
-                    '僕の強みを聞いてみる',
-                    style: TextStyle(
-                        color: Color.fromRGBO(50, 50, 50, 1),
-                        fontWeight: FontWeight.bold),
-                  )),
               Container(
                 decoration: const BoxDecoration(
                   image: DecorationImage(
@@ -128,6 +114,25 @@ class SummaryState extends State<Summary> {
                   ),
                 ),
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                      onPressed: _onPressed,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromRGBO(219, 171, 188, 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(1000),
+                        ),
+                      ),
+                      child: const Text(
+                        '僕の強みを聞いてみる',
+                        style: TextStyle(
+                            color: Color.fromRGBO(50, 50, 50, 1),
+                            fontWeight: FontWeight.bold),
+                      )),
+                ],
+              )
             ],
           ),
           Padding(
