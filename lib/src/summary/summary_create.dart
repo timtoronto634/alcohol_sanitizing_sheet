aba3048f0c31ae8fdac2d6a96fb8c498e1d30dd9
timtoronto634 <tim225x@gmail.com>
2023-10-15 10:22:34 +0900

    feat: add chat ui

--- lib/src/summary/summary_create.dart
+++ lib/src/summary/summary_create.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:alcohol_sanitizing_sheet/src/helper.dart/db_helper.dart';
+import 'package:alcohol_sanitizing_sheet/src/summary/dialog_create.dart';
 import 'package:flutter/material.dart';
 import 'package:dart_openai/dart_openai.dart';
 import 'package:just_audio/just_audio.dart';
@@ -76,57 +77,64 @@ class SummaryState extends State<Summary> {
           fit: BoxFit.cover,
         ),
       ),
-      child: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: _onPressed,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(219, 171, 188, 1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(1000),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            children: [
+              ElevatedButton(
+                  onPressed: _onPressed,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(219, 171, 188, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(1000),
+                    ),
                   ),
-                ),
-                child: const Text(
-                  '僕の強みを聞いてみる',
-                  style: TextStyle(
-                      color: Color.fromRGBO(50, 50, 50, 1),
-                      fontWeight: FontWeight.bold),
-                )),
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/fuuka_arima.png'),
-                  // fit: BoxFit.cover,
-                ),
-              ),
-              constraints: const BoxConstraints(
-                minHeight: 650,
-              ),
-              child: Container(
-                width: 450,
+                  child: const Text(
+                    '僕の強みを聞いてみる',
+                    style: TextStyle(
+                        color: Color.fromRGBO(50, 50, 50, 1),
+                        fontWeight: FontWeight.bold),
+                  )),
+              Container(
                 decoration: const BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage('assets/images/box.png'),
-                    alignment: Alignment.bottomCenter,
+                    image: AssetImage('assets/images/fuuka_arima.png'),
+                    // fit: BoxFit.cover,
                   ),
                 ),
-                padding: const EdgeInsets.only(
-                    top: 260, bottom: 18, left: 32, right: 32),
-                constraints: const BoxConstraints(),
+                constraints: const BoxConstraints(
+                  maxHeight: 300,
+                ),
                 child: Container(
-                  height: 300,
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: _isLoading
-                        ? const CircularProgressIndicator()
-                        : Text(_summary),
+                  width: 450,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/box.png'),
+                      alignment: Alignment.bottomCenter,
+                    ),
+                  ),
+                  padding: const EdgeInsets.only(
+                      top: 260, bottom: 18, left: 32, right: 32),
+                  constraints: const BoxConstraints(),
+                  child: Container(
+                    height: 300,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: _isLoading
+                          ? const CircularProgressIndicator()
+                          : Text(_summary),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MessageForm(),
+          ),
+        ],
       ),
     );
   }
