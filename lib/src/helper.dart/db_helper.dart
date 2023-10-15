0930e3b11611b5ddb7b2d19ff553c3e62431b604
timtoronto634 <tim225x@gmail.com>
2023-10-15 12:25:09 +0900

    add initial data when no data

--- lib/src/helper.dart/db_helper.dart
+++ lib/src/helper.dart/db_helper.dart
@@ -3,13 +3,25 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class DBHelper {
+  static List<Diary> initialData = [
+    Diary(
+        content: '仕事お疲れ様、今日はどんな感じだった？ 今日は新しいプロジェクトが始まって大変だったよ。',
+        date: DateTime.now()),
+    Diary(
+        content: 'どんなプロジェクトなの？ 複数の会社でゲームを作ることになったんだけど、先方のエンジニアがコミュニケーションが下手で',
+        date: DateTime.now()),
+    Diary(
+        content:
+            'あなたはどうしたの？ 相手に寄り添って話を聞いて、信頼を勝ち取ったんだ。そこからはスムーズに意見を交わし合えるようになった',
+        date: DateTime.now()),
+  ];
+
   static Future<Database> database() async {
     print("database");
     final dbPath = await getDatabasesPath();
     return openDatabase(join(dbPath, 'myDiary.db'), onCreate: (db, version) {
       return db.execute(
-        'CREATE TABLE diaries(id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, date TEXT)',
-      );
+          'CREATE TABLE diaries(id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, date TEXT);');
     }, version: 1);
   }
 
@@ -38,7 +50,13 @@ class DBHelper {
   static Future<List<Diary>> fetchDiaries() async {
     print("fetchDiaries");
     final db = await DBHelper.database();
-    final result = await db.query('diaries');
+    var result = await db.query('diaries');
+    if (result.isEmpty) {
+      for (var diary in initialData) {
+        await insertDiary(diary);
+      }
+      result = await db.query('diaries');
+    }
     return result.map((e) => Diary.fromMap(e)).toList();
   }
 }
